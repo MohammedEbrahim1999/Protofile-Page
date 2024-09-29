3d01169e91427d09e23e2dbaf0f3008330c8680b
Mohammed Ebrahim <92871678+MohammedEbrahim1999@users.noreply.github.com>
2024-09-30 02:21:28 +0300

    Update myProjects.js

--- src/components/3-main/myProjects.js
+++ src/components/3-main/myProjects.js
@@ -1,20 +1,122 @@
 const myProjects = [
-    { projectTitle: "Logan", category: "html", imgPath: "/imgs/1.jpg" },
+    // 1
     {
-        projectTitle: "Construction Company",
-        category: "html",
-        imgPath: "/imgs/2.jpg",
+        projectTitle: "E-Commerce Website",
+        githubLink: "https://github.com/MohammedEbrahim1999/FrontEnd",
+        category: "Mui",
+        imgPath: "/imgs/E-Commerce.png",
+        projectDes: "I Create This Project With React & Matrial Ui And Use BackEnd Strapi In This Project",
+        uses: "Technologies Used :React,MUi,JavaScript,HTML,CSS",
+        domainLink: "https://front-end-indol-tau.vercel.app",
     },
-    { projectTitle: "Protofile ", category: "js", imgPath: "/imgs/3.jpg" },
+    // 2
     {
-        projectTitle: "Hospital management System ",
-        category: "js",
-        imgPath: "/imgs/4.jpg",
+        projectTitle: "Senaei",
+        githubLink: "https://github.com/MohammedEbrahim1999/senaei",
+        category: "next",
+        imgPath: "/imgs/Senaei.png",
+        projectDes: "I made This Project As FreeLance To TURNQY Comapny,I Work With Them For Three Months ",
+        uses: "Technologies Used :NextJs , Tailwind Css",
+        domainLink: "https://senaei-wbxf.vercel.app/",
     },
-    { projectTitle: "Shopping Cart ", category: "react", imgPath: "/imgs/5.jpg" },
+    // 3
     {
-        projectTitle: "Protofile Page ",
+        projectTitle: "DNK Store",
+        githubLink: "https://github.com/MohammedEbrahim1999/dnk-store",
+        category: "next",
+        imgPath: "/imgs/DNK.png",
+        projectDes: "I Made This DNK Store With Nextjs To Learn More And Add It To My Works ",
+        uses: "Technologies Used :NextJs , Css ,JavaScript",
+        domainLink: "https://dnk-store-njp1.vercel.app/",
+    },
+    // 4
+    {
+        projectTitle: "Earth Store",
+        githubLink: "https://github.com/MohammedEbrahim1999/earth-store",
+        category: "next",
+        imgPath: "/imgs/Earth Store.png",
+        projectDes: "I Made This Earth Store With Nextjs To Learn More And Add It To My Works ",
+        uses: "Technologies Used :NextJs , Css ,JavaScript",
+        domainLink: "https://earth-store-red.vercel.app/",
+    },
+    // 5
+    {
+        projectTitle: "Prayer",
+        githubLink: "https://github.com/MohammedEbrahim1999/Prayer",
         category: "react",
-        imgPath: "/imgs/6.jpg",
+        imgPath: "/imgs/prayer.png",
+        projectDes: "i Used An API TO Load Data About prayer And Add it TO The Project ",
+        uses: "Technologies Used :React, Css, HTML, APILINK",
+        domainLink: "https://prayer-ashy.vercel.app/",
+    },
+    // 6
+    {
+        projectTitle: "Food Era",
+        githubLink: "https://github.com/MohammedEbrahim1999/FoodEra",
+        category: "react",
+        imgPath: "/imgs/FoodEra.png",
+        projectDes: "I Made This Project With React To Get More Experience With React Framework",
+        uses: "Technologies Used :React, Css, HTML",
+        domainLink: "https://food-era.vercel.app/",
+    },
+    // 7
+    {
+        projectTitle: "Kasper",
+        githubLink: "https://github.com/MohammedEbrahim1999/Kasper",
+        category: "html",
+        imgPath: "/imgs/Kasper.png",
+        projectDes: "I made This Project Using HTML And CSS Only To Practise On It",
+        uses: "Technologies Used : HTML,Css",
+        domainLink: "https://kasper-pi-umber.vercel.app/",
+    },
+    // 8
+    {
+        projectTitle: "Hospital Management System",
+        githubLink: "https://github.com/MohammedEbrahim1999/Hospital-management-system",
+        category: "js",
+        imgPath: "/imgs/HMS.png",
+        projectDes: "This Was My Graduation Project On 2021 I Work On it With All Team",
+        uses: "Technologies Used :HTML, Css, javaScript",
+        domainLink: "https://hospital-management-system-three-pink.vercel.app/",
+    },
+    // 9
+    {
+        projectTitle: "Protofilo",
+        githubLink: "https://github.com/MohammedEbrahim1999/Protofile",
+        category: "js",
+        imgPath: "/imgs/ProtoFilo.png",
+        projectDes: "I Made This Project With HTML,CSS and JS , I Use LocalStorage With This Project",
+        uses: "Technologies Used :HTML, Css, javaScript",
+        domainLink: "https://protofile-six.vercel.app/",
+    },
+    // 10
+    {
+        projectTitle: "Logan",
+        githubLink: "https://github.com/MohammedEbrahim1999/Logan-main",
+        category: "html",
+        imgPath: "/imgs/Logan.png",
+        projectDes: "I made This Project Using HTML And CSS Only To Practise On It",
+        uses: "Technologies Used :HTML, Css",
+        domainLink: "https://logan-main.vercel.app/",
+    },
+    // 11
+    {
+        projectTitle: " Construction Company",
+        githubLink: "https://github.com/MohammedEbrahim1999/Construction-Comapny",
+        category: "html",
+        imgPath: "/imgs/Construction.png",
+        projectDes: "I made This Project Using HTML And CSS Only To Practise On It",
+        uses: "Technologies Used :HTML, Css",
+        domainLink: "",
+    },
+    // 12
+    {
+        projectTitle: "Protofile Page",
+        githubLink: "https://github.com/MohammedEbrahim1999/Protofile-Page",
+        category: "react",
+        imgPath: "/imgs/",
+        projectDes: "This Is My Protofile Page That Contain All Projects I Made And What I Will Do Next",
+        uses: "Technologies Used :React, Css, HTML",
+        domainLink: "",
     },
 ];
