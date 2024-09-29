dc6dc1814e046f4fd31471e57234651410088683
Mohammed Ebrahim <92871678+MohammedEbrahim1999@users.noreply.github.com>
2024-09-30 02:17:16 +0300

    Update Main.jsx

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -2,49 +2,125 @@ import React, { useState } from "react";
 import "./main.css";
 import { AnimatePresence, motion } from "framer-motion";
 const myProjects = [
-    {   projectTitle: "Logan", 
-        githublink: "https://github.com/MohammedEbrahim1999/Logan", 
-        category: "html", 
-        imgPath: "/imgs/1.jpg" ,
-        projectDes: "I worked on the project to design a Responsive website. This was the first Responsive website I made, and I finished it ten months ago and it Give Me More data About Responsive design",
-        uses: " Technologies Used: HTML,CSS,JavaScript,Bootstrap",
-    },
+    // 1
     {
-        projectTitle: "Construction Company",
-        githublink: "https://github.com/MohammedEbrahim1999/Construction-Company",
-        category: "html",
-        imgPath: "/imgs/2.jpg",
-        projectDes: "I started learning With made this project and finished it in a short period of time.I tried to make it responsive.",
-        uses: " Technologies Used: HTML,CSS,",
+        projectTitle: "E-Commerce Website",
+        githubLink: "https://github.com/MohammedEbrahim1999/FrontEnd",
+        category: "Mui",
+        imgPath: "/imgs/E-Commerce.png",
+        projectDes: "I Create This Project With React & Matrial Ui And Use BackEnd Strapi In This Project",
+        uses: "Technologies Used :React,MUi,JavaScript,HTML,CSS",
+        domainLink: "https://front-end-indol-tau.vercel.app",
     },
-    {   projectTitle: "Protofile ",
-        githublink: "https://github.com/MohammedEbrahim1999/Protofile", 
-        category: "js", 
-        imgPath: "/imgs/3.jpg",
-        projectDes: "I Made This Website Two Months Ago to Grow My Projects and I Add A lot Of Features Using Js. ",
-        uses : "Technologies Used: HTML,CSS,JavaScript,Jquery, Es6",
-    },
+    // 2
     {
-        projectTitle: "Hospital management System ",
-        githublink: "https://github.com/MohammedEbrahim1999/Hospital-management-system",
-        category: "js",
-        imgPath: "/imgs/4.jpg",
-        projectDes: "I participated in this project while working on my graduation project from the university in 2021, and it was discussed with the relevant committee",
-        uses :  "Technologies Used: HTML,CSS,JavaScrip",
+        projectTitle: "Senaei",
+        githubLink: "https://github.com/MohammedEbrahim1999/senaei",
+        category: "next",
+        imgPath: "/imgs/Senaei.png",
+        projectDes: "I made This Project As FreeLance To TURNQY Comapny,I Work With Them For Three Months ",
+        uses: "Technologies Used :NextJs , Tailwind Css",
+        domainLink: "https://senaei-wbxf.vercel.app/",
     },
-    {   projectTitle: "Shopping Cart ", 
-        githublink: "https://github.com/MohammedEbrahim1999/Shopping-Cart", 
-        category: "react", 
-        imgPath: "/imgs/5.jpg",
-        projectDes: "I made This Project With React FrameWork And It Similar to a food store for ordering food, but it is an incomplete version, I was learning React at the time ",
-        uses : "Technologies Used: HTML, CSS, Js, React"
-    },
+    // 3
     {
-        projectTitle: "Protofile Page ",
+        projectTitle: "DNK Store",
+        githubLink: "https://github.com/MohammedEbrahim1999/dnk-store",
+        category: "next",
+        imgPath: "/imgs/DNK.png",
+        projectDes: "I Made This DNK Store With Nextjs To Learn More And Add It To My Works ",
+        uses: "Technologies Used :NextJs , Css ,JavaScript",
+        domainLink: "https://dnk-store-njp1.vercel.app/",
+    },
+    // 4
+    {
+        projectTitle: "Earth Store",
+        githubLink: "https://github.com/MohammedEbrahim1999/earth-store",
+        category: "next",
+        imgPath: "/imgs/Earth Store.png",
+        projectDes: "I Made This Earth Store With Nextjs To Learn More And Add It To My Works ",
+        uses: "Technologies Used :NextJs , Css ,JavaScript",
+        domainLink: "https://earth-store-red.vercel.app/",
+    },
+    // 5
+    {
+        projectTitle: "Prayer",
+        githubLink: "https://github.com/MohammedEbrahim1999/Prayer",
         category: "react",
-        imgPath: "/imgs/6.jpg",
-        projectDes: "This Is My Protofile Page That Contain All Projects I Made And What I Will Do Next, Contain My Cv Also And I Will Make This Page Up To Date Always        ",
-        uses : "Technologies Used: HTML, CSS, Js, React"
+        imgPath: "/imgs/prayer.png",
+        projectDes: "i Used An API TO Load Data About prayer And Add it TO The Project ",
+        uses: "Technologies Used :React, Css, HTML, APILINK",
+        domainLink: "https://prayer-ashy.vercel.app/",
+    },
+    // 6
+    {
+        projectTitle: "Food Era",
+        githubLink: "https://github.com/MohammedEbrahim1999/FoodEra",
+        category: "react",
+        imgPath: "/imgs/FoodEra.png",
+        projectDes: "I Made This Project With React To Get More Experience With React Framework",
+        uses: "Technologies Used :React, Css, HTML",
+        domainLink: "https://food-era.vercel.app/",
+    },
+    // 7
+    {
+        projectTitle: "Kasper",
+        githubLink: "https://github.com/MohammedEbrahim1999/Kasper",
+        category: "html",
+        imgPath: "/imgs/Kasper.png",
+        projectDes: "I made This Project Using HTML And CSS Only To Practise On It",
+        uses: "Technologies Used : HTML,Css",
+        domainLink: "https://kasper-pi-umber.vercel.app/",
+    },
+    // 8
+    {
+        projectTitle: "Hospital Management System",
+        githubLink: "https://github.com/MohammedEbrahim1999/Hospital-management-system",
+        category: "js",
+        imgPath: "/imgs/HMS.png",
+        projectDes: "This Was My Graduation Project On 2021 I Work On it With All Team",
+        uses: "Technologies Used :HTML, Css, javaScript",
+        domainLink: "https://hospital-management-system-three-pink.vercel.app/",
+    },
+    // 9
+    {
+        projectTitle: "Protofilo",
+        githubLink: "https://github.com/MohammedEbrahim1999/Protofile",
+        category: "js",
+        imgPath: "/imgs/ProtoFilo.png",
+        projectDes: "I Made This Project With HTML,CSS and JS , I Use LocalStorage With This Project",
+        uses: "Technologies Used :HTML, Css, javaScript",
+        domainLink: "https://protofile-six.vercel.app/",
+    },
+    // 10
+    {
+        projectTitle: "Logan",
+        githubLink: "https://github.com/MohammedEbrahim1999/Logan-main",
+        category: "html",
+        imgPath: "/imgs/Logan.png",
+        projectDes: "I made This Project Using HTML And CSS Only To Practise On It",
+        uses: "Technologies Used :HTML, Css",
+        domainLink: "https://logan-main.vercel.app/",
+    },
+    // 11
+    {
+        projectTitle: " Construction Company",
+        githubLink: "https://github.com/MohammedEbrahim1999/Construction-Comapny",
+        category: "html",
+        imgPath: "/imgs/Construction.png",
+        projectDes: "I made This Project Using HTML And CSS Only To Practise On It",
+        uses: "Technologies Used :HTML, Css",
+        domainLink: "",
+    },
+    // 12
+    {
+        projectTitle: "Protofile Page",
+        githubLink: "https://github.com/MohammedEbrahim1999/Protofile-Page",
+        category: "react",
+        imgPath: "/imgs/",
+        projectDes: "This Is My Protofile Page That Contain All Projects I Made And What I Will Do Next",
+        uses: "Technologies Used :React, Css, HTML",
+        domainLink: "",
     },
 ];
 const Main = () => {
@@ -60,15 +136,7 @@ const Main = () => {
     return (
         <main className="flex" id="projects">
             <section className="flex left-section">
-                <button
-                    onClick={() => {
-                        setcurActive("all");
-                        setArr(myProjects);
-                    }}
-                    className={curActive === "all" ? "active" : null}
-                >
-                    All Projects
-                </button>
+                <button onClick={() => { setcurActive("all"); setArr(myProjects); }} className={curActive === "all" ? "active" : null}> All Projects </button>
                 <button
                     onClick={() => {
                         handleClick("html");
@@ -94,6 +162,22 @@ const Main = () => {
                 >
                     React
                 </button>
+                <button
+                    onClick={() => {
+                        handleClick("next");
+                    }}
+                    className={curActive === "next" ? "active" : null}
+                >
+                    NextJs
+                </button>
+                <button
+                    onClick={() => {
+                        handleClick("Mui");
+                    }}
+                    className={curActive === "Mui" ? "active" : null}
+                >
+                    React & MUI
+                </button>
             </section>
             <section className="right-section flex ">
                 <AnimatePresence>
@@ -105,7 +189,7 @@ const Main = () => {
                                 animate={{ transform: "scale(1)" }}
                                 transition={{ type: "spring", damping: 20, stiffness: 30 }}
                                 key={item.imgPath} className="card">
-                                <img width={283} src={item.imgPath} alt="" />
+                                <img width={283} src={item.imgPath} alt="ProjectImg" />
                                 <div style={{ width: "283" }} className=" box">
                                     <h1 className="land1-title">{item.projectTitle}</h1>
                                     <p className="land1-subtitle">
@@ -113,14 +197,14 @@ const Main = () => {
                                     </p>
                                     <div className="land1-icons flex">
                                         <div className="flex land1-icons-first">
-                                            <a href="item.link" target="_blank">
+                                            <a href={item.domainLink} target="_blank">
                                                 <div className="icon-link"></div>
                                             </a>
-                                            <a href={item.githublink} target="_blank">
+                                            <a href={item.githubLink} target="_blank">
                                                 <div className="icon-github"></div>
                                             </a>
                                         </div>
-                                        <a href="#contact" className="more-icon flex">
+                                        <a href={item.domainLink} target="_blank" className="more-icon flex">
                                             More
                                             <span
                                                 style={{ alignSelf: "end" }}
